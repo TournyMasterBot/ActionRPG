5154703e9bd06f5aca246264bcabf9f0cdb079b5
TournyMasterBot <TournyMasterBot@users.noreply.github.com>
2022-08-27 08:15:38 -0500

    Move protos to folder and cleanup gen script (#12)
    * Move protos to folder and cleanup gen script

--- generate_proto_files.ps1
+++ generate_proto_files.ps1
@@ -2,51 +2,71 @@
 # `./generate_proto_files.ps1`
 
 # Fetch all proto files frop the gen path and output them to the server location and game asset folder
-$dir = "./Server/ProtosGen/" # This is the path to your grpc_csharp_plugin.exe and protos file definitions
-$files = Get-ChildItem -Path $dir -File '*.proto' | Select-Object Name
-$serverProtosPath = "./Server/ActionRpg.Server.Grpc/Protos" # This is the ActionRpg server folder path
-$gameProtosPath = "./Game/ActionRpg/Assets/Protos" # This is the Unity3D game for ActionRpg
-$gameClientProtosPath = "./Client/ActionRpg.Client.GameClient/Protos" # This is the test game client for ActionRpg
+$basePath = Split-Path $MyInvocation.MyCommand.Path
+echo "Base path: $basePath"
+$dir = Convert-Path "$basePath/Server/ProtosGen" # This is the path to your grpc_csharp_plugin.exe and protos file definitions
+echo "Protos path: $dir"
+echo "Creating Generated Input Path"
+New-Item -ItemType Directory -Force -Path "$dir/GeneratedInput"
+$inputPath = Convert-Path "$dir/GeneratedInput/"
+echo "Creating Generated Output Path"
+New-Item -ItemType Directory -Force -Path "$dir/GeneratedOutput"
+$outputPath = Convert-Path "$dir/GeneratedOutput/"
+echo "Output Path: $outputPath"
+$serverProtosPath = Convert-Path "$basePath/Server/ActionRpg.Server.Grpc/Protos" # This is the ActionRpg server folder path
+echo "Sever Protos Path: $serverProtosPath"
+$gameProtosPath = Convert-Path "$basePath/Game/ActionRpg/Assets/ActionRpgAssets/Protos" # This is the Unity3D game for ActionRpg
+echo "Game Protos Path: $gameProtosPath"
+$gameClientProtosPath = Convert-Path "$basePath/Client/ActionRpg.Client.GameClient/Protos" # This is the test game client for ActionRpg
+echo "Game Client Protos Path: $gameClientProtosPath"
 
+# Create output locations
+echo "Creating Server Protos Path: $serverProtosPath"
 New-Item -ItemType Directory -Force -Path $serverProtosPath
+echo "Creating Game Protos Path: $gameProtosPath"
 New-Item -ItemType Directory -Force -Path $gameProtosPath
+echo "Creating Game Client Protos Path: $gameClientProtosPath"
 New-Item -ItemType Directory -Force -Path $gameClientProtosPath
 
-# Game
-foreach ($file in $files) {
-	# Delete proto if it already exists
-	$delpath = $gameProtosPath + $file.Name
-	if (Test-Path $delpath) {
-		Remove-Item -Path -Force $delpath
-	}
-	
-	# Write the new proto
-    $gameGen = "./Server/ProtosGen/protoc.exe -I $dir --csharp_out=$gameProtosPath --grpc_out=$gameProtosPath --plugin=protoc-gen-grpc=$dir/grpc_csharp_plugin.exe " +$file.Name
-    Invoke-Expression $gameGen
+# Delete existing items
+Remove-Item "$inputPath*.*"
+Remove-Item "$outputPath*.*"
+Remove-Item "$serverProtosPath*.*"
+Remove-Item "$gameProtosPath*.*"
+Remove-Item "$gameClientProtosPath*.*"
+
+# Fetch source files
+echo "Consolidating input files"
+$inputFiles = Get-ChildItem -Path $dir -Recurse -File '*.proto'
+foreach ($file in $inputFiles) {
+	$filepath = $file.FullName
+	$fileName = $file.Name
+	echo "Copying to input path: $inputPath, file name: $fileName"
+	$copyInputPath = $inputPath + $fileName
+	Copy-Item -Path $filepath -Destination $copyInputPath 
 }
 
-# Server
-foreach($file in $files) {
-	# Delete proto if it already exists
-	$delpath = $gameProtosPath + $file.Name
-	if (Test-Path $delpath) {
-		Remove-Item -Path -Force $delpath
-	}
-	
+# Create the protoc files at output location
+echo "Generating output files"
+$files = Get-ChildItem -Path $inputPath -Recurse -File '*.proto' | Select-Object Name
+foreach ($file in $files) {	
 	# Write the new proto
-    $serverGen = "./Server/ProtosGen/protoc.exe -I $dir --csharp_out=$serverProtosPath --grpc_out=$serverProtosPath --plugin=protoc-gen-grpc=$dir/grpc_csharp_plugin.exe " + $file.Name
-    Invoke-Expression $serverGen
+	$filePath = $file.FullName
+	$fileName = $file.Name
+	$protoGen = "$dir/protoc.exe -I $inputPath --csharp_out=$outputPath --grpc_out=$outputPath --plugin=protoc-gen-grpc=$dir/grpc_csharp_plugin.exe " + $file.Name
+	echo "Protoc output: $outputPath$fileName"
+	Invoke-Expression $protoGen
 }
 
-# Game Client
-foreach ($file in $files) {
-	# Delete proto if it already exists
-	$delpath = $gameClientProtosPath + $file.Name
-	if (Test-Path $delpath) {
-		Remove-Item -Path -Force $delpath
-	}
-	
-	# Write the new proto
-    $clientGen = "./Server/ProtosGen/protoc.exe -I $dir --csharp_out=$gameClientProtosPath --grpc_out=$gameClientProtosPath --plugin=protoc-gen-grpc=$dir/grpc_csharp_plugin.exe " +$file.Name
-    Invoke-Expression $clientGen
+# Copy files to new locations
+$outputFiles = Get-ChildItem -Path $outputPath -Recurse -File '*.cs'
+foreach($file in $outputFiles) {
+	$filePath = $file.FullName
+	$fileName = $file.Name
+	echo "Exporting file to $serverProtosPath$fileName"
+	Copy-Item -Path $filepath -Destination $serverProtosPath
+	echo "Exporting file to $gameProtosPath$fileName"
+	Copy-Item -Path $filepath -Destination $gameProtosPath
+	echo "Exporting file to $gameClientProtosPath$fileName"
+	Copy-Item -Path $filepath -Destination $gameClientProtosPath
 }
