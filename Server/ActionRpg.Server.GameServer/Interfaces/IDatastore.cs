60cec793a806f618036388a9357835927805ec04
TournyMasterBot <TournyMasterBot@users.noreply.github.com>
2022-08-27 20:20:49 -0500

    Problematic Files

--- Server/ActionRpg.Server.GameServer/Interfaces/IDatastore.cs
+++ Server/ActionRpg.Server.GameServer/Interfaces/IDatastore.cs
@@ -1,4 +1,10 @@
-﻿namespace ActionRpg.Datastore.Interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionRpg.Server.GameServer.Interfaces
 {
     public interface IDatastore
     {
