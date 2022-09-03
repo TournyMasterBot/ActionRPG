2c83b3cea54c950c026b2b18e03eb323bb90bf4c
TournyMasterBot <TournyMasterBot@users.noreply.github.com>
2022-09-03 13:02:05 -0500

    Add more seed content and auto create simple storage tables

--- Server/ActionRpg.Server.Grpc/Program.cs
+++ Server/ActionRpg.Server.Grpc/Program.cs
@@ -1,14 +1,18 @@
 using ActionRpg.Core.Helpers;
 using ActionRpg.Models.DatastoreCoreModels;
+using ActionRpg.Models.Interfaces;
 using ActionRpg.Models.ItemModels;
 using ActionRpg.Server.GameServer.Converter;
 using ActionRpg.Server.GameServer.Loaders;
 using ActionRpg.Server.GameServer.Managers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ActionRpg.Server.Grpc
 {
@@ -23,69 +27,30 @@ namespace ActionRpg.Server.Grpc
                 AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
                 ApplicationState.Init("beta"); // Todo : Support args to switch beta/prod
                 log = new LoggingHelpers("Program");
-                // Todo : Move datastore creation to relevant datastore area
-                ApplicationState.App.Datastore.CreateTable(new CreateTableInput()
-                {
-                    TableName = "items",
-                    Fields = new TableFieldInput[2]
-                    {
-                    new TableFieldInput()
-                    {
-                        FieldName = "key",
-                        FieldType = "string",
-                        MaxLength = 100,
-                        IsPrimaryKey = true,
-                        IsNullable = false,
-                    },
-                    new TableFieldInput()
-                    {
-                        FieldName = "data",
-                        FieldType = "text",
-                        IsNullable = false,
-                    }
-                    },
-                    TableIndexes = new TableIndexInput[1]
-                    {
-                    new TableIndexInput()
-                    {
-                        IndexName = "uix_items_key",
-                        IndexType = Models.ServerConstants.TableIndexType.Unique,
-                        Fields = new string[1] { "key" }
-                    }
-                    }
-                });
                 var gameDataFiles = GameDataLoader.LoadFileList(Path.GetFullPath($"{ApplicationState.App.Config.ApplicationBasePath}/GameData")); // Todo : Add flag args to enable / disable loading data during server start
                 var gameDataMap = GameDataLoader.LoadData(gameDataFiles);
                 var gameItems = new List<Item>(); // Todo : Handle game data map loading elsewhere
+                // Todo : Move datastore creation to relevant datastore area
+                var requiredTables = gameDataMap.Select(x => x.Key).Distinct().ToArray();
+                foreach(var table in requiredTables)
+                {
+                    ApplicationState.App.Datastore.CreateSimpleTable(table);
+                }
+                var saveItemList = new List<SaveItemInput<string>>(); // Todo: Move this to a more relevant area for datastore handling
                 foreach (var dataMap in gameDataMap)
                 {
-                    switch (dataMap.Key)
+                    foreach(var row in dataMap.Value)
                     {
-                        case "items":
+                        dynamic data = JsonConvert.DeserializeObject<dynamic>(row);
+                        saveItemList.Add(new SaveItemInput<string>()
                         {
-                            foreach (var item in dataMap.Value)
-                            {
-                                gameItems.Add(item.JsonToGameItem());
-                            }
-                            break;
-                        }
-                        default:
-                        {
-                            throw new NotImplementedException($"Unknown datamap key: {dataMap.Key}. Count: {dataMap.Value.Length}");
-                        }
+                            TableName = dataMap.Key,
+                            Key = data["id"].Value,
+                            Value = row
+                        });
                     }
                 }
-                var saveItemList = new List<SaveItemInput<Item>>(); // Todo: Move this to a more relevant area for datastore handling
-                foreach (var item in gameItems)
-                {
-                    saveItemList.Add(new SaveItemInput<Item>()
-                    {
-                        TableName = "items",
-                        Key = item.ItemID,
-                        Value = item
-                    });
-                }
-                ApplicationState.App.Datastore.SaveListToDatastore<Item>(saveItemList.ToArray());
+                ApplicationState.App.Datastore.SaveListToDatastore<string>(saveItemList.ToArray());
                 CreateHostBuilder(args).Build().Run();
             }
             finally
